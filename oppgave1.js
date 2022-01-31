523ca1e40da8140834490ff93847e428d7b7cf2e
Renatebh <renate.hem@gmail.com>
2022-01-31 11:23:18 +0100

    second commit

--- oppgave1.js
+++ oppgave1.js
@@ -1,3 +1,26 @@
+class Animal {
+  constructor(spieces, name, country, color, imgUrl) {
+    this.spieces = spieces;
+    this.name = name;
+    this.country = country;
+    this.color = color;
+  }
+}
 
+const animalOne = new Animal(
+  "kattedyr",
+  "løve",
+  "africa",
+  "yellow",
+  "https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045_960_720.jpg"
+);
 
+console.log(animalOne.name);
 
+const animalTwo = new Animal(
+  "fugl",
+  "måke",
+  "norge",
+  "hvit og grå",
+  "https://cdn.pixabay.com/photo/2018/02/16/22/08/bird-3158784_960_720.jpg"
+);
